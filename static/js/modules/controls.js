820c5a46d677cb281a03912c26fd84181e5e3fb0
pikesley <sam.pikesley@gmail.com>
2021-10-17 14:58:05 +0100

    Nuke very-redundant cruft

--- static/js/modules/controls.js
+++ static/js/modules/controls.js
@@ -75,57 +75,6 @@ let setStyles = function () {
   }
 };
 
-// let SpanManager = class {
-//   constructor(current, next) {
-//     this.current = current || [];
-//     this.next = next || [];
-
-//     this.activate = this.difference(this.next, this.current);
-//     this.deactivate = this.difference(this.current, this.next);
-
-//     this.diffs = false;
-
-//     if (this.activate.length || this.deactivate.length) {
-//       this.diffs = true;
-//     }
-//   }
-
-//   yeet() {
-//     if (this.diffs) {
-//       this.activate.forEach(function (span) {
-//         let elements = document.querySelectorAll(span);
-//         elements.forEach(function (element) {
-//           element.classList.remove("inactive");
-//           element.classList.add("active");
-//         });
-//       });
-
-//       this.deactivate.forEach(function (span) {
-//         let elements = document.querySelectorAll(span);
-//         elements.forEach(function (element) {
-//           element.classList.remove("active");
-//           element.classList.add("inactive");
-//         });
-//       });
-
-//       this.save();
-//     }
-//   }
-
-//   save() {
-//     console.log(this.next.join(" "));
-//     localStorage["active-classes"] = JSON.stringify(this.next);
-//   }
-
-//   // https://stackoverflow.com/a/30288946
-//   difference(left, right) {
-//     right = new Set(right);
-//     return left.filter(function (x) {
-//       return !right.has(x);
-//     });
-//   }
-// };
-
 let TimeFinder = class {
   constructor() {
     this.actual = new Date();
