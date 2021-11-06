2cdd429d53ad847ca446c0b74c9802d752b668fe
pikesley <sam.pikesley@gmail.com>
2021-11-06 14:43:15 +0000

    I somehow lost a load of work from last night
    Don't be committing at 01:00 on a Saturday morning. I'm too old for this shit.

--- controller.py
+++ controller.py
@@ -45,9 +45,34 @@ def cycle_style():
 @app.route("/reload", methods=["POST"])
 def reload():
     """Reload the screen."""
+    if "PLATFORM" in os.environ:
+        if os.environ["PLATFORM"] == "docker":
+            return {"status": "OK", "platform": "docker"}
+
     os.environ["DISPLAY"] = ":0"
     subprocess.run(("xdotool key F5").split(" "), check=True)
     sleep(1)
+    return {"status": "OK"}
+
+
+@app.route("/style", methods=["GET"])
+def get_style():
+    """Get the language."""
+    style = app.redis.get("style")
+
+    if style:
+        style = style.decode()
+    else:
+        style = "blue-orange"
+
+    return {"style": style}
+
+
+@app.route("/style", methods=["POST"])
+def set_style():
+    """Set the language."""
+    app.redis.set("style", request.json["style"])
+    reload()
 
     return {"status": "OK"}
 
