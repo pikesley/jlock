8cf7d0549afc02036a8df38187306fe2e40fbe60
pikesley <sam.pikesley@gmail.com>
2021-11-06 23:28:39 +0000

    Also make `en` fill a 12x10 grid
    This has broken the `offset` Sass

--- static/js/modules/internationalisation/languages/en.js
+++ static/js/modules/internationalisation/languages/en.js
@@ -1,35 +1,48 @@
 export default {
   name: "English",
   data: [
-    [{ class: "it" }, { text: "l" }, { class: "is" }, { text: "asampm" }],
-    [{ class: "a" }, { text: "c" }, { class: "quarter" }, { text: "dc" }],
-    [{ class: "twenty" }, { class: "five" }, { text: "x" }],
+    [{ class: "it" }, { text: "l" }, { class: "is" }, { text: "asampmt" }],
+    [
+      { class: "quarter", text: "a" },
+      { text: "c" },
+      { class: "quarter" },
+      { text: "dic" },
+    ],
+    [
+      { classes: ["twentyfive", "twenty"], text: "twenty" },
+      { classes: ["twentyfive", "five"], text: "five" },
+      { text: "xu" },
+    ],
     [
       { class: "half" },
       { text: "s" },
       { class: "ten" },
-      { text: "f" },
+      { text: "fr" },
       { class: "to" },
     ],
-    [{ class: "past" }, { text: "eru" }, { class: "h-9", text: "nine" }],
+    [{ class: "past" }, { text: "egru" }, { class: "h-9", text: "nine" }],
     [
       { class: "h-1", text: "one" },
       { class: "h-6", text: "six" },
+      { text: "j" },
       { class: "h-3", text: "three" },
     ],
     [
       { class: "h-4", text: "four" },
+      { text: "d" },
       { class: "h-5", text: "five" },
       { class: "h-2", text: "two" },
     ],
     [
       { class: "h-8", text: "eight" },
+      { text: "k" },
       { class: "h-11", text: "eleven" },
     ],
     [
       { class: "h-7", text: "seven" },
+      { text: "c" },
       { class: "h-0", text: "twelve" },
     ],
-    [{ class: "h-10", text: "ten" }, { text: "se" }, { class: "oclock" }],
+    [{ class: "h-10", text: "ten" }, { text: "sae" }, { class: "oclock" }],
   ],
 };
