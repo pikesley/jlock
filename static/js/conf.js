664427b9bd3361479906b35b83a8957feabe76ec
pikesley <sam.pikesley@gmail.com>
2021-11-07 16:31:29 +0000

    Move `width` to `conf`

--- static/js/conf.js
+++ static/js/conf.js
@@ -1,5 +1,6 @@
 let conf = {
   spacers: 1, // this should match `clock-spacers` in `sass/base/_vars.scss`
+  width: 12, // this should match `clock-width` in `sass/base/_vars.scss`
   fadeIncrement: 0.1,
   dot: "•",
 };
