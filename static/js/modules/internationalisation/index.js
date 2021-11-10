ad7088b09e0d6800d1aa79e0201d2932fa81c830
pikesley <sam.pikesley@gmail.com>
2021-11-10 21:22:13 +0000

    Attempt to make a Spanish clock

--- static/js/modules/internationalisation/index.js
+++ static/js/modules/internationalisation/index.js
@@ -1,9 +1,11 @@
 import * as en from "./languages/en.js";
 import * as cy from "./languages/cy.js";
+import * as es from "./languages/es.js";
 
 let languages = {
   en: en.default,
   cy: cy.default,
+  es: es.default,
 };
 
 export { languages };
