d08eca3470f449f8819d000ef28afbfa7ee156cf
pikesley <sam.pikesley@gmail.com>
2021-12-14 21:11:37 +0000

    Increase `git reset` aggression levels

--- scripts/updater.sh
+++ scripts/updater.sh
@@ -2,11 +2,11 @@ CURRENT_HEAD=$(git rev-parse HEAD)
 
 git fetch
 git reset --hard origin/main
+git clean -fxd
 
 NEW_HEAD=$(git rev-parse HEAD)
 
-if [ "${NEW_HEAD}" != "${CURRENT_HEAD}" ]
-then
+if [ "${NEW_HEAD}" != "${CURRENT_HEAD}" ]; then
   ./configure
   make
 fi
