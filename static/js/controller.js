57dd6529f9f389f4537599de256f90783e0f1290
pikesley <sam.pikesley@gmail.com>
2021-12-17 21:53:57 +0000

    Refactor loads of this
    Possibly I can make some of testable

--- static/js/controller.js
+++ static/js/controller.js
@@ -19,6 +19,85 @@ let firstRun = {
 
 let currents = {};
 
+let select = function (parameter, id) {
+  if (currents[parameter] == id) {
+    return;
+  }
+
+  socket.send({ [parameter]: id });
+
+  resizeIndicator(parameter, id);
+  yeetIndicator(parameter, id);
+  selectify(parameter, id);
+  currents[parameter] = id;
+};
+
+let selectify = function (parameter, id) {
+  try {
+    document
+      .getElementById(makeID(parameter, currents[parameter]))
+      .classList.remove("selected");
+  } catch (TypeError) {}
+  document.getElementById(makeID(parameter, id)).classList.add("selected");
+};
+
+let yeetIndicator = function (parameter, id) {
+  let deltas = getDeltas(parameter, id);
+  indicators[
+    parameter
+  ].style.transform = `translateX(${deltas.x}) translateY(${deltas.y})`;
+};
+
+let resizeIndicator = function (parameter, id) {
+  ["height", "width"].forEach(function (attribute) {
+    setAttribute(parameter, id, attribute);
+  });
+};
+
+let locateIndicator = function (parameter, id) {
+  ["top", "left"].forEach(function (attribute) {
+    setAttribute(parameter, id, attribute);
+  });
+
+  resizeIndicator(parameter, id);
+  selectify(parameter, id);
+};
+
+let initialiseIndicator = function (parameter, json) {
+  if (firstRun[parameter]) {
+    let id = json[parameter];
+    locateIndicator(parameter, id);
+    currents[parameter] = id;
+    firstRun[parameter] = false;
+  }
+};
+
+let getDeltas = function (parameter, id) {
+  let button = buttons[parameter][id];
+  return {
+    x: addUnits(button.left - stripUnits(indicators[parameter].style.left)),
+    y: addUnits(button.top - stripUnits(indicators[parameter].style.top)),
+  };
+};
+
+let makeID = function (left, right) {
+  return `${left}-${right}`;
+};
+
+let addUnits = function (value) {
+  return `${value}px`;
+};
+
+let stripUnits = function (value) {
+  return value.replace(/(.*)px/, "$1");
+};
+
+let setAttribute = function (parameter, id, attribute) {
+  indicators[parameter].style[attribute] = addUnits(
+    buttons[parameter][id][attribute]
+  );
+};
+
 document.querySelectorAll(".btn").forEach(function (button) {
   button.addEventListener("click", function () {
     select(button.dataset.type, button.dataset.name);
@@ -28,78 +107,13 @@ document.querySelectorAll(".btn").forEach(function (button) {
     button.getBoundingClientRect();
 });
 
-let select = function (parameter, id) {
-  if (currents[parameter] == id) {
-    return;
-  }
-
-  socket.send({ [parameter]: id });
-
-  let button = buttons[parameter][id];
-  let deltaX = button.left - stripUnits(indicators[parameter].style.left);
-  let deltaY = button.top - stripUnits(indicators[parameter].style.top);
-
-  ["height", "width"].forEach(function (attribute) {
-    indicators[parameter].style[
-      attribute
-    ] = `${buttons[parameter][id][attribute]}px`;
-  });
-
-  indicators[
-    parameter
-  ].style.transform = `translateX(${deltaX}px) translateY(${deltaY}px)`;
-
-  document
-    .getElementById(makeID(parameter, currents[parameter]))
-    .classList.remove("selected");
-  document.getElementById(makeID(parameter, id)).classList.add("selected");
-
-  currents[parameter] = id;
-};
-
-let stripUnits = function (value) {
-  return value.replace(/(.*)px/, "$1");
-};
-
-let locateIndicator = function (parameter, data) {
-  ["top", "left"].forEach(function (attribute) {
-    indicators[parameter].style[attribute] = `${
-      buttons[parameter][data[parameter]][attribute]
-    }px`;
-  });
-
-  ["height", "width"].forEach(function (attribute) {
-    indicators[parameter].style[attribute] = `${
-      buttons[parameter][data[parameter]][attribute]
-    }px`;
-  });
-
-  document
-    .getElementById(makeID(parameter, data[parameter]))
-    .classList.add("selected");
-};
-
-let makeID = function (left, right) {
-  return `${left}-${right}`;
-};
-
-socket.on("style", function (json) {
-  if (firstRun["style"]) {
-    locateIndicator("style", json);
-    currents["style"] = json["style"];
-    firstRun["style"] = false;
-  }
-});
-
-socket.on("language", function (json) {
-  if (firstRun["language"]) {
-    locateIndicator("language", json);
-    currents["language"] = json["language"];
-    firstRun["language"] = false;
-  }
-});
-
 document.getElementById("show-metadata").addEventListener("click", function () {
   let element = document.getElementById("git-metadata-modal");
   element.classList.toggle("visible");
+});
+
+["style", "language"].forEach(function (parameter) {
+  socket.on(parameter, function (json) {
+    initialiseIndicator(parameter, json);
+  });
 });
